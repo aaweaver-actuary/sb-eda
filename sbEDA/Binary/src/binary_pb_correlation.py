645f9990a6b0141f393a53e07d75c3bc0a279669
Andy Weaver <andrewayersweaver@gmail.com>
2023-07-27 14:21:54 -0400

    finished binary_pb_correlation.py + test checks out

--- sbEDA/Binary/src/binary_pb_correlation.py
+++ sbEDA/Binary/src/binary_pb_correlation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import scipy.stats as stats
 
-def binary_pb_correlation(target:pd.Series, features:pd.Series) -> float:
+def binary_pb_correlation_test(target:pd.Series, features:pd.Series) -> float:
     """
     Calculates the point biserial correlation between a binary target
     and binary features.
@@ -18,4 +18,56 @@ def binary_pb_correlation(target:pd.Series, features:pd.Series) -> float:
     float
         Point biserial correlation between target and features.
     """
-    return stats.pointbiserialr(target, features)
+    return stats.pointbiserialr(target, features)[0]
+
+def binary_pb_correlation_p_value(target:pd.Series, features:pd.Series) -> float:
+    """
+    Calculates the point biserial correlation between a binary target
+    and binary features, and returns the p-value.
+    
+    Parameters
+    ----------
+    target : pd.Series
+        Binary target column.
+    features : pd.Series
+        Binary feature column.
+
+    Returns
+    -------
+    float
+        Point biserial correlation p-value between target and features.
+    """
+    return stats.pointbiserialr(target, features)[1]
+
+def binary_pb_correlation_hypothesis_test(target:pd.Series,
+                                          features:pd.Series,
+                                          alpha_levels:list=None
+                                         ) -> pd.DataFrame:
+    """
+    Calculates the point biserial correlation between a binary target and
+    binary features, and returns a dataframe with the correlation, p-value,
+    and whether the null hypothesis is rejected at a variety of alpha levels.
+
+    Parameters
+    ----------
+    target : pd.Series
+        Binary target column.
+    features : pd.Series
+        Binary feature column.
+    alpha_levels : list, optional
+        List of alpha levels to test, by default None
+
+    Returns
+    -------
+    pd.DataFrame
+        Point biserial correlation, p-value, and hypothesis test results.
+    """
+    if alpha_levels is None:
+        alpha_levels = [0.001, 0.01, 0.05, 0.1, 0.2, 0.25, 0.5]
+
+    results = pd.DataFrame(columns=['significance_level'])
+    results['significance_level'] = alpha_levels
+    results['correlation'] = binary_pb_correlation_test(target, features)
+    results['p_value'] = binary_pb_correlation_p_value(target, features)
+    results['reject_null'] = results['p_value'] < results['significance_level']
+    return results
