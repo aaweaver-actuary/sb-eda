d2bf4775877d47d70cde0d388c2bef0f83ce5238
aaweaver-actuary <andrewayersweaver+github@gmail.com>
2023-08-16 02:42:17 -0400

    done with charts for continuous feature/binary predictor

--- sbEDA/Binary/plot.py
+++ sbEDA/Binary/plot.py
@@ -10,6 +10,9 @@ import seaborn as sns
 from matplotlib.ticker import FuncFormatter
 from matplotlib.colors import ListedColormap
 
+# import matplotlib ax for type annotations
+from matplotlib.axes import Axes
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_curve, auc
 import statsmodels.api as sm
@@ -84,40 +87,38 @@ def _format_plot(feature,
         ax.set_xlabel(feature_t)
 
     
-    if legend:
-        # set the labels for the legend
-        ax.legend(title=target_t)
-    
     return ax
 
-def binary_distribution_plot(feature,
-                             target,
-                             df,
-                             ax=None,
-                             title="Distribution of feature by target",
-                             figsize=None,
-                             save_fig=False,
-                             save_path=None,
-                             **kwargs):
+def binary_distribution_plot(feature: pd.Series,
+                             target: pd.Series,
+                             ax: Axes = None,
+                             title: str = "Distribution of feature by target",
+                             figsize: tuple = None,
+                             save_fig: bool = False,
+                             save_path: str = None,
+                             **kwargs) -> Axes:
     """
     Plot the distribution of the feature split by the target variable.
 
     Parameters
     ----------
-    feature : str
-        The name of the feature to plot.
-    target : str
-        The name of the target variable.
-    df : pandas.DataFrame
-        The dataframe containing the feature and target variable.
+    feature : pandas.Series
+        The feature to plot.
+    target : pandas.Series
+        The target variable.
     ax : matplotlib.axes.Axes, optional
-        The matplotlib axes to plot on.
+        The matplotlib axes to plot on. If None, a new figure and axes are
+        created.
     title : str, optional
         The title of the plot. The keywords "feature" and "target" will be
         replaced with the actual feature and target names.
         It is done this way to keep the function as general as possible.
     figsize : tuple, optional
         The size of the figure. If None, the default of (15, 8) is used.
+    save_fig : bool, optional
+        Whether to save the figure. Default is False.
+    save_path : str, optional
+        The path to save the figure to. If None, the default of "plots" is used.
     **kwargs : dict
         Keyword arguments to pass to seaborn.histplot.
 
@@ -135,62 +136,45 @@ def binary_distribution_plot(feature,
         _, ax = plt.subplots(figsize=figsize)        
 
     # plot the distribution
-    sns.histplot(data=df,
-                    x=feature,
-                    hue=target,
-                    ax=ax,
-                    kde=True,
-                    fill=True,
-                    stat="density",
-                    **kwargs)
+    sns.histplot(x=feature,
+                 hue=target,
+                 ax=ax,
+                 kde=True,
+                 fill=True,
+                 stat="density",
+                 **kwargs)
     
     # format the plot
-    ax = _format_plot(feature, target, ax=ax, title=title, n_series=1, legend=False)
+    ax = _format_plot(feature.name,
+                      target.name,
+                      ax=ax,
+                      title=title,
+                      n_series=1,
+                      legend=False)
 
     if save_fig:
         if save_path is None:
             save_path = "plots"
-        plt.savefig(f"{save_path}/{feature}_distribution.png",
+        plt.savefig(f"{save_path}/{feature.name}_distribution.png",
                     dpi=300,
                     bbox_inches="tight")
-        
 
     return ax
-    
 
-
-
-
-def binary_boxplot(feature,
-                   target,
-                   df,
-                   ax=None,
-                   title="Binary Boxplot of feature by target",
-                   figsize=None,
-                   save_fig=False,
-                   save_path=None,
-                   **kwargs):
+def binary_boxplot(feature: pd.Series,
+                   target: pd.Series,
+                   ax: Axes = None,
+                   title: str = "Binary Boxplot of feature by target",
+                   figsize: tuple = None,
+                   save_fig: bool = False,
+                   save_path: str = None,
+                   **kwargs) -> Axes:
     """
     Plot a boxplot of the feature split by the target variable.
     
     Parameters
     ----------
-    feature : str
-        The name of the feature to plot.
-    target : str
-        The name of the target variable.
-    df : pandas.DataFrame
-        The dataframe containing the feature and target variable.
-    ax : matplotlib.axes.Axes, optional
-        The matplotlib axes to plot on.
-    title : str, optional
-        The title of the plot. The keywords "feature" and "target" will be
-        replaced with the actual feature and target names.
-        It is done this way to keep the function as general as possible.
-    figsize : tuple, optional
-        The size of the figure. If None, the default of (15, 8) is used.
-    **kwargs : dict
-        Keyword arguments to pass to seaborn.boxplot.
+    
     
     Returns
     -------
@@ -208,34 +192,69 @@ def binary_boxplot(feature,
     # plot the boxplot
     sns.boxplot(x=target,
                 y=feature,
-                data=df,
                 ax=ax,
                 **kwargs)
     
-    ax = _format_plot(feature, target, ax=ax, title=title, n_series=2)
+    ax = _format_plot(feature.name, target.name, ax=ax, title=title, n_series=2)
 
     if save_fig:
         if save_path is None:
             save_path = "plots"
-        plt.savefig(f"{save_path}/{feature}_distribution.png",
+        plt.savefig(f"{save_path}/{feature.name}_distribution.png",
                     dpi=300,
                     bbox_inches="tight")
 
     return ax
 
-def binary_roc_auc(feature, target, alpha=0.05, figsize=_figsize, save_fig=False, save_path=None):
+def binary_roc_auc(feature: pd.Series,
+                   target: pd.Series,
+                   ax: Axes = None,
+                   alpha: float = 0.05,
+                   figsize: tuple = _figsize,
+                   save_fig: bool = False,
+                   save_path: str = None) -> Axes:
+    """
+    Plot the ROC curve and AUC for a single-variable logistic regression model.
 
+    Parameters
+    ----------
+    feature : pandas.Series
+        The feature to plot.
+    target : pandas.Series
+        The target variable.
+    ax : matplotlib.axes.Axes, optional
+        The matplotlib axes to plot on. If None, a new figure and axes are
+        created.
+    alpha : float, optional
+        The significance level. Default is 0.05.
+    figsize : tuple, optional
+        The size of the figure. If None, the default of (15, 8) is used.
+    save_fig : bool, optional
+        Whether to save the figure. Default is False.
+    save_path : str, optional
+        The path to save the figure to. If None, the default of "plots" is used.
 
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        The matplotlib axes containing the ROC curve and AUC plot.
+    """
+    # get the figure size
+    if figsize is None:
+        figsize = _figsize
 
-    # Reshape feature for scikit-learn
-    feature = feature.values.reshape(-1, 1)
+    # get the axes
+    if ax is None:
+        _, ax = plt.subplots(figsize=figsize)
+
+    feature_name = feature.name
 
     # Fit logistic regression model
     log_reg = LogisticRegression()
-    log_reg.fit(feature, target)
+    log_reg.fit(feature.values.reshape(-1,1), target)
 
     # Predict probability
-    probas = log_reg.predict_proba(feature)[:, 1]
+    probas = log_reg.predict_proba(feature.values.reshape(-1,1))[:,1]
 
     # Compute ROC curve and AUC
     fpr, tpr, _ = roc_curve(target, probas)
@@ -250,36 +269,79 @@ def binary_roc_auc(feature, target, alpha=0.05, figsize=_figsize, save_fig=False
     s_band = _get_significance_band(p_value, 'coefficient')
 
     # Plot ROC curve
-    plt.figure(figsize=_figsize)
-    plt.plot(fpr, tpr, color='darkorange', label='ROC curve (AUC = %0.2f)' % roc_auc)
-    plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title(f'Receiver Operating Characteristic from \
+    # plt.figure(figsize=_figsize)
+    ax.plot(fpr, tpr, color='darkorange', label='ROC curve (AUC = %0.2f)' % roc_auc)
+    ax.plot([0, 1], [0, 1], color='navy', linestyle='--')
+    ax.set_xlim([0.0, 1.0])
+    ax.set_ylim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title(f'Receiver Operating Characteristic from \
 Single-Variable Logistic Regression\n\
 [Coefficient]={logit_model.params[1]:.3f}, \
-[p-value]={p_value:0.4f}, \
+[p-value]={p_value:0.4f}\n\
 {s_band}')
-    plt.legend(loc='lower right')
+    ax.legend(loc='lower right')
 
     # Annotate plot with p-value and other details if needed
     significance = "Significant" if p_value < alpha else "Not Significant"
-    annotation = f"p-value: {p_value:.3f}\n{significance}\nCoefficient: {logit_model.params[1]:.3f}"
-    plt.annotate(annotation, xy=(0.6, 0.2), xycoords='axes fraction')
+    annotation = f"p-value: {p_value:.3f}\n{significance}\n\
+Coefficient: {logit_model.params[1]:.3f}"
+    ax.annotate(annotation, xy=(0.6, 0.2), xycoords='axes fraction')
 
-    plt.show()
+    if save_fig:
+        if save_path is None:
+            save_path = "plots"
+        plt.savefig(f"{save_path}/{feature_name}_roc_auc.png",
+                    dpi=300,
+                    bbox_inches="tight")
+        
+    # return the axes
+    return ax
 
 
-def plot_ks_statistic(feature,
-                      target,
-                      alpha=0.05,
-                      figsize=_figsize,
-                      save_fig=False,
-                      save_path=None):
-    # Create figure
-    plt.figure(figsize=figsize)    
+def binary_ks_plot(feature: pd.Series,
+                   target: pd.Series,
+                   ax: Axes = None,
+                   alpha: float = 0.05,
+                   figsize: tuple = _figsize,
+                   save_fig: bool = False,
+                   save_path: str = None) -> Axes:
+    """
+    Plot the Kolmogorov-Smirnov statistic for a feature split by the target
+    variable.
+
+    Parameters
+    ----------
+    feature : pandas.Series
+        The feature to plot.
+    target : pandas.Series
+        The target variable.
+    ax : matplotlib.axes.Axes, optional
+        The matplotlib axes to plot on. If None, a new figure and axes are
+        created.
+    alpha : float, optional
+        The significance level. Default is 0.05.
+    figsize : tuple, optional
+        The size of the figure. If None, the default of (15, 8) is used.
+    save_fig : bool, optional
+        Whether to save the figure. Default is False.
+    save_path : str, optional
+        The path to save the figure to. If None, the default of "plots" is used.
+
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        The matplotlib axes containing the Kolmogorov-Smirnov plot.
+    """
+    
+    # get the figure size
+    if figsize is None:
+        figsize = _figsize
+
+    # get the axes
+    if ax is None:
+        _, ax = plt.subplots(figsize=figsize)
 
     # Separate feature by target classes
     class_0 = feature[target == 0]
@@ -309,11 +371,11 @@ def plot_ks_statistic(feature,
         significance_statement2 = significance_statement
 
     # Plot CDFs
-    plt.plot(x_range,
+    ax.plot(x_range,
              [cdf_0(x) for x in x_range],
              label='class 0 CDF'.replace("class", target.name).replace("_", " ").title(),
              color='navy')
-    plt.plot(x_range,
+    ax.plot(x_range,
              [cdf_1(x) for x in x_range],
              label='class 1 CDF'.replace("class", target.name).replace("_", " ").title(),
              color='darkorange')
@@ -336,5 +398,16 @@ def plot_ks_statistic(feature,
                     dpi=300,
                     bbox_inches="tight")
 
-    plt.show()
+    # return the axes
+    return plt.gca()
 
+def binary_plots(df: pd.DataFrame,
+                 target: str,
+                 feature: str,):
+    _, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(17, 17))
+    ax1 = binary_distribution_plot(df[feature], df[target], ax=ax1)
+    ax2 = binary_boxplot(df[feature], df[target], ax=ax2)
+    ax3 = binary_roc_auc(df[feature], df[target], ax=ax3)
+    ax4 = binary_ks_plot(df[feature], df[target], ax=ax4)
+    plt.tight_layout()
+    return plt.gca()
